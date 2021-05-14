0c4c086c37637047a70ad2503c9991eced389787
moris <44795768+morimoris@users.noreply.github.com>
2021-05-14 11:22:09 +0900

    Update model.py

--- model.py
+++ model.py
@@ -19,20 +19,20 @@ def DRCN(recursive_depth, input_channels, filter_num = 256):
     conv2d_0 = Conv2D(filters = filter_num, kernel_size = (3, 3), padding = "same", activation = "relu")(input_shape)
     conv2d_1 = Conv2D(filters = filter_num, kernel_size = (3, 3), padding = "same", activation = "relu")(conv2d_0)                       
     #Inference net and Reconstruction net.
-    weight_list = []
-    pred_list = []
+    weight_list = recursive_depth * [None]
+    pred_list = recursive_depth * [None]
 
     for i in range(recursive_depth):
         Inferencd_output = Inferencd_conv2d(conv2d_1)
         Recon_0 = Conv2D(filters = filter_num, kernel_size = (3, 3), padding = "same", activation = "relu")(Inferencd_output)
         Recon_1 = Conv2D(filters = input_channels, kernel_size = (3, 3), padding = "same", activation = "relu")(Recon_0)
-        weight_list.append(Recon_1)
+        weight_list[i] = Recon_1
     
     for i in range(recursive_depth):
         skip_connection = Add()([weight_list[i], input_shape])
         pred = Multiply()([weight_list[i], skip_connection])
 
-        pred_list.append(pred)
+        pred_list[i] = pred
 
     pred = Add()(pred_list)
 
